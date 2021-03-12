6df46daafbbe019c0e212e27b788043e6ec2c9bc
baldo-vela <baldo.vela+github@gmail.com>
2021-03-12 16:51:50 -0600

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,17 +1,17 @@
 
-INSERT INTO BEARS (name, gender)
+INSERT INTO BEARS (name, sex)
     VALUES ("Mr. Chocolate", "M");
-INSERT INTO BEARS (name, gender)
+INSERT INTO BEARS (name, sex)
     VALUES ("Rowdy", "F");
-INSERT INTO BEARS (name, gender)
+INSERT INTO BEARS (name, sex)
     VALUES ("Tabitha", "F");
-INSERT INTO BEARS (name, gender)
+INSERT INTO BEARS (name, sex)
     VALUES ("Sergeant Brown", "M");
-INSERT INTO BEARS (name, gender)
+INSERT INTO BEARS (name, sex)
     VALUES ("Melissa", "F");
-INSERT INTO BEARS (name, gender)
+INSERT INTO BEARS (name, sex)
     VALUES ("Grinch", "M");
-INSERT INTO BEARS (name, gender)
+INSERT INTO BEARS (name, sex)
     VALUES ("Wendy", "F");
-    INSERT INTO BEARS (name, gender)
+INSERT INTO BEARS (name, sex)
     VALUES (NULL, NULL);
